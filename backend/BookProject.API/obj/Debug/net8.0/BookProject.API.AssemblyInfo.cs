5ad40aa230401f02ff4307c08850d41ad15a5116
Riley Wells <rwells9@byu.edu>
2025-03-31 09:56:31 -0600

    Updating to Phase03

--- backend/BookProject.API/obj/Debug/net8.0/BookProject.API.AssemblyInfo.cs
+++ backend/BookProject.API/obj/Debug/net8.0/BookProject.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookProject.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+632388902ae980b69dd88fb5d41e5043a890159b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookProject.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookProject.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
