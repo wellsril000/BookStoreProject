ffbe8192b6b11124bea6a04c60960041e1d4694b
Riley Wells <rwells9@byu.edu>
2025-04-01 17:51:25 -0600

    Updating backend

--- backend/BookProject.API/obj/Debug/net8.0/BookProject.API.AssemblyInfo.cs
+++ backend/BookProject.API/obj/Debug/net8.0/BookProject.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookProject.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52e97b5388defe5b46df908d49405be8bf8dc1fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5f8317684688c788ac423c175502e8f329d18ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookProject.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookProject.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
